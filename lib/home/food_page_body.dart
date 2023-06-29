816c2203ac227a94e0ee8a88b570396560695ca8
Shreyas Shetty <shreyasshetty170@gmail.com>
2023-06-29 23:37:45 +0530

    Fifth Commit Created the sliding dots indicator also created the dimensions.dart file to set dynamically adjust the contents on the screen so that it should work on any device flawlessly.

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,9 +1,12 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/icon_and_text_widget.dart';
 import 'package:food_delivery/widgets/small_text.dart';
+
+import '../utils/dimensions.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({super.key});
@@ -15,7 +18,7 @@ class FoodPageBody extends StatefulWidget {
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
   var _currPageValue = 0.0;
-  double _height = 220;
+  double _height = Dimensions.pageViewContainer;
   double _scaleFactor = 0.8;
 
   @override
@@ -36,14 +39,28 @@ class _FoodPageBodyState extends State<FoodPageBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 320,
-      child: PageView.builder(
-        controller: pageController,
-          itemCount: 5,
-          itemBuilder: (context,position){
-        return _buildPageItem(position);
-      }),
+    return Column(
+      children: [
+        Container(
+          height: Dimensions.pageView,
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: 5,
+            itemBuilder: (context,position){
+              return _buildPageItem(position);
+            }),
+        ),
+        DotsIndicator(
+          dotsCount: 5,
+          position: _currPageValue,
+          decorator: DotsDecorator(
+            activeColor: AppColors.mainColor,
+          size: const Size.square(9.0),
+          activeSize: const Size(18.0, 9.0),
+          activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          ),
+        ),
+      ],
     );
   }
   Widget _buildPageItem(int index){
@@ -74,7 +91,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       child: Stack(
         children: [
           Container(
-            height: 220,
+            height: Dimensions.pageViewContainer,
             margin: EdgeInsets.only(left: 10,right: 10 ),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
@@ -90,20 +107,34 @@ class _FoodPageBodyState extends State<FoodPageBody> {
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              height: 120,
+              height: Dimensions.pageViewTextContainer,
               margin: EdgeInsets.only(left: 30,right: 30, bottom: 30),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 color: Colors.white,
-
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(0,5)
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset:Offset(-5, 0)
+                  ),
+                  BoxShadow(
+                      color: Colors.white,
+                      offset:Offset(5, 0)
+                  ),
+                ]
               ),
               child: Container(
-                padding: EdgeInsets.only(top: 15,left: 15,right: 15),
+                padding: EdgeInsets.only(top: Dimensions.height15,left: 15,right: 15),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     BigText(text: "Indian Side"),
-                    SizedBox(height: 10),
+                    SizedBox(height: Dimensions.height10),
                     Row(
                       children: [
                         Wrap(
@@ -117,8 +148,9 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                         SmallText(text: "comments"),
                       ],
                     ),
-                    SizedBox(height: 20,),
+                    SizedBox(height: Dimensions.height20),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         IconAndTextWidget(icon: Icons.circle_sharp,
                             text: "Normal",
